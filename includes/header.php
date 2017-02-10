d46ebdc347ea655880186423b839becad62470b4
Nicolas <info.gwma@gmail.com>
2017-02-10 11:43:14 +0100

    Main menu modification in includes/header.php. Adding store for responsive.Adding logo on the home page.

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,21 @@
 
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
-        <ul class="nav navbar-nav">
-            <li><a href="../index.php">Home</a></li>
-            <li><a href="../blog/bloghome.php">Blog</a></li>
-            <li><a href="../chat/chathome.php">Chat</a></li>
-        </ul>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="main-menu">
+            <ul class="nav navbar-nav">
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../blog/bloghome.php">Blog</a></li>
+                <li><a href="../chat/chathome.php">Chat</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
 
--- index.php
+++ index.php
@@ -1,19 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: USER
- * Date: 26/10/2016
- * Time: 10:06
- */
+
 include_once 'includes/connect.php';
 include_once 'includes/head.php';
 include_once 'includes/header.php';
-include_once 'includes/body.php';
 ?>
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-12">
-            <h1>Home</h1>
+            <img id="logoPhp" class="img-responsive" src="assets/logo_php.svg" />
         </div>
     </div>
 </div>
